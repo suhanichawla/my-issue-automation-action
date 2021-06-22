5964a64777bd0646c86fa7dc67de26778199a479
Suhani Chawla <suhanichawla2000@gmail.com>
2021-06-22 23:30:57 +0530

    thank you on PR

--- src/action.js
+++ src/action.js
@@ -1,5 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
     console.log("Hello world!")
+    const GITHUB_TOKEN = core.getInput(GITHUB_TOKEN)
+    const octokit =  github.getOctokit(GITHUB_TOKEN)
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    console.log("context",context)
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: 'Thank you for the PR'
+    })
 }
 
 run()
