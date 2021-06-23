c857bec92122020dcecc579497d8706bb85a1aac
Suhani Chawla <suhanichawla2000@gmail.com>
2021-06-24 01:25:47 +0530

    added task checker

--- src/action.js
+++ src/action.js
@@ -9,13 +9,50 @@ async function run() {
     const { context = {} } = github;
     const { issue } = context.payload;
     console.log("context",context)
-    console.log("issue",issue)
+    console.log("issue",issue.body)
 
+    var rbody= removeIgnoreTaskLitsText(issue.body)
+    const text = createTaskListText(rbody)
+    console.log("text is", text)
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: issue.number,
-        body: 'Thank you for the issue'
+        body: text
     })
 }
+
+function removeIgnoreTaskLitsText(text) {
+    return text.replace(
+      /<!-- ignore-task-list-start -->[\s| ]*(- \[[x| ]\] .+[\s| ]*)+<!-- ignore-task-list-end -->/g,
+      ''
+    )
+  }
+  
+function createTaskListText(body) {
+    const completedTasks = body.match(/(- \[[x]\].+)/g)
+    const uncompletedTasks = body.match(/(- \[[ ]\].+)/g)
+  
+    let text = ''
+  
+    if (completedTasks !== null) {
+      for (let index = 0; index < completedTasks.length; index++) {
+        if (index === 0) {
+          text += '## :white_check_mark: Completed Tasks\n'
+        }
+        text += `${completedTasks[index]}\n`
+      }
+    }
+  
+    if (uncompletedTasks !== null) {
+      for (let index = 0; index < uncompletedTasks.length; index++) {
+        if (index === 0) {
+          text += '## :x: Uncompleted Tasks\n'
+        }
+        text += `${uncompletedTasks[index]}\n`
+      }
+    }
+  
+    return text
+  }
 
 run()
